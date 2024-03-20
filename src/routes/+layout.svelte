4160bc8364151a4cb167c684c445625501fa5e5e
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-03-20 20:40:05 +0100

    Add hero section
    only with text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,18 @@
 </script>
 
 <nav>
-	<img src="akademia-title.svg" alt="" />
-	<a href="">Download</a>
+	<a href="" class="mr-3">
+		<img src="akademia-title.svg" alt="" />
+	</a>
+	<a href="" class="button hover:bg-muted">Download</a>
+	<a href="" class="button hover:bg-muted">Lær mere</a>
 	<div class="spacer"></div>
+	<a href="" class="button hover:bg-muted">login</a>
+	<a href="" class="button bg-primary hover:bg-primary-muted">Sign up</a>
 </nav>
 <slot />
 
-<style>
+<style lang="scss">
 	img {
 		/* width: 100%; */
 		height: 3rem;
@@ -23,7 +28,15 @@
 		display: flex;
 
 		align-items: center;
-		padding: 0.75rem 0.8rem;
-		gap: 0.5rem;
+		padding: 0.75rem 2rem;
+		gap: 0.7rem;
+	}
+
+	.button {
+		font-size: larger;
+		font-weight: 500;
+		padding: 0.25rem 0.75rem;
+		@apply rounded-md;
+		text-align: center;
 	}
 </style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="header py-40 px-10">
+	<h1 class="text-7xl font-bold leading-[5rem] max-w-[40rem]">
+		Opgaver, Lektier, Noter Alt samlet i én platform
+	</h1>
+</div>
+
+<style>
+</style>
