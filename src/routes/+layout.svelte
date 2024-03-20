8be9084fe930fd0eb768df94b3ada96bda0a9203
Elliott <85990359+Ell1ott@users.noreply.github.com>
2024-03-20 19:50:53 +0100

    add logo to navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,29 @@
 <script lang="ts">
-  import '../app.scss';
+	import '../app.scss';
 </script>
 
+<nav>
+	<img src="akademia-title.svg" alt="" />
+	<a href="">Download</a>
+	<div class="spacer"></div>
+</nav>
 <slot />
+
+<style>
+	img {
+		/* width: 100%; */
+		height: 3rem;
+	}
+
+	.spacer {
+		flex: 1;
+	}
+
+	nav {
+		display: flex;
+
+		align-items: center;
+		padding: 0.75rem 0.8rem;
+		gap: 0.5rem;
+	}
+</style>
