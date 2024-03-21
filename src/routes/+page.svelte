1ef7758c97bf5c97ac4f7197a918ddabc0ba494d
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2024-03-21 11:17:26 +0100

    Add screenshot image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,20 +2,27 @@
 	import { ArrowDown } from 'lucide-svelte';
 </script>
 
-<div class="header py-40 px-24 space-y-8">
-	<h1 class="text-7xl font-bold leading-[5rem] max-w-[40rem]">
-		<span class="shadow-blue-700">Opgaver</span>,
-		<span class="shadow-purple-700">Lektier</span>,
-		<span class="shadow-emerald-700">Noter</span>
-		alt samlet i én platform
-	</h1>
-	<div class="flex gap-10 items-center text-xl">
-		<button class="button shadow primary text-xl px-6 py-1 rounded-md"> Prøv det! </button>
-		<a href="" class="flex gap-1 whitespace-nowrap relative items-center">
-			Lær mere
-			<ArrowDown size="19"></ArrowDown>
-		</a>
+<div class="header py-32 px-24 flex gap-20 relative">
+	<div class="space-y-8">
+		<h1 class="text-7xl font-bold leading-[5rem] max-w-[41rem]">
+			<span class="shadow-blue-700">Opgaver</span>,
+			<span class="shadow-purple-700">Lektier</span>,
+			<span class="shadow-emerald-700">Noter</span>
+			alt samlet i én platform
+		</h1>
+		<div class="flex gap-10 items-center text-xl">
+			<button class="button shadow primary text-xl px-6 py-1 rounded-md"> Prøv det! </button>
+			<a href="" class="flex gap-1 whitespace-nowrap relative items-center">
+				Lær mere
+				<ArrowDown size="19"></ArrowDown>
+			</a>
+		</div>
 	</div>
+	<img
+		src="screenshot.png"
+		alt="Screenshot of Akademia"
+		class="absolute left-[50rem] h-[25rem] top-[50%] -transform -translate-y-[50%] border rounded-md"
+	/>
 </div>
 
 <style lang="scss">
@@ -50,11 +57,16 @@
 	.shadow {
 		box-shadow: rgb(#000, 0.2) -2px 2px 8px;
 
-		transition: all 0.3s;
+		transition: all 0.6s;
 
-		&:hover {
+		&:not(:active):hover {
+			transition: all 0.3s;
 			box-shadow: rgb(#000, 0.2) -4px 6px 10px;
 			transform: translate(2px, -4px);
+		}
+
+		&:active {
+			transition: all 0.15s;
 		}
 	}
 </style>
