d2cddb71e3cee7f9b349d4ab53cf8e4d5f38c132
Jonathan Bangert <jonathan@bangert.dk>
2024-03-24 23:37:45 +0100

    remove manual height

--- src/lib/contactForm.svelte
+++ src/lib/contactForm.svelte
@@ -17,12 +17,12 @@
             <Input type="text" name="last_name" placeholder="Efternavn" />
         </div>
         <Input type="email" name="email" placeholder="Email" />
-        <Textarea name="message" placeholder="Besked" class="h-32" />
+        <Textarea name="message" placeholder="Besked" />
         <Input type="text" name="honeypot" style="display:none" />
         <input type="hidden" name="accessKey" value="530612c8-1297-4f59-a4cf-d88352ffb570">
         <input type="hidden" name="subject" value="Contact us from - akademia.cc" />
         <input type="hidden" name="replyTo" value="@">
         <input type="hidden" name="redirectTo" value="https://akademia.cc/">
-        <Button type="submit" class="text-base w-48 h-12">Send besked</Button>
+        <Button type="submit" class="text-lg w-48 h-12">Send besked</Button>
     </form>
 </div>
