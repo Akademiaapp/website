e31e04fb8bb717c01743e28e40e0c53fac0b9926
Jonathan Bangert <jonathan@bangert.dk>
2024-03-25 15:38:08 +0100

    Custom 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,28 @@
+<div>
+    <h1>
+        404 - Siden blev ikke fundet
+    </h1>
+    <p>
+        Siden du leder efter, blev ikke fundet. Prøv at gå tilbage til forsiden.
+    </p>
+</div>
+
+<style lang="scss">
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 80vh;
+    }
+
+    h1 {
+        font-size: 3rem;
+        margin-bottom: 1rem;
+    }
+
+    p {
+        font-size: 1.5rem;
+        text-align: center;
+    }
+</style>
