b910c05005dc20410334e4ab6519d7c6346f629e
Jonathan Bangert <jonathan@bangert.dk>
2025-07-01 15:56:12 +0200

    Update contactForm.svelte

--- src/lib/contactForm.svelte
+++ src/lib/contactForm.svelte
@@ -6,7 +6,7 @@
 
 <div class="p-10">
 	<form
-		action="https://api.staticforms.xyz/submit"
+		action=""
 		method="post"
 		class="flex h-full flex-col gap-4 lg:max-w-[20rem] xl:max-w-[30rem] 2xl:max-w-[40rem]"
 	>
@@ -22,7 +22,7 @@
 		<Input type="email" name="email" placeholder="Email" />
 		<Textarea name="message" placeholder="Besked" class="min-h-20 flex-1 resize-none" />
 		<Input type="text" name="honeypot" style="display:none" />
-		<input type="hidden" name="accessKey" value="530612c8-1297-4f59-a4cf-d88352ffb570" />
+		<input type="hidden" name="accessKey" value="" />
 		<input type="hidden" name="subject" value="Contact us from - akademia.cc" />
 		<input type="hidden" name="replyTo" value="@" />
 		<input type="hidden" name="redirectTo" value="https://akademia.cc/" />
